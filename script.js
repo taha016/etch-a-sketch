39b1377e6ed16f90375f429e6dfef65706082e64
taha016 <msaddiqueshareef@gmail.com>
2025-01-02 21:12:23 +0500

    Added interactive slider for sketch pad's square numbers.

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const container  = document.querySelector(".container");
+const container = document.querySelector(".container");
 
-let rows = document.querySelector(".rows");
+var slider = document.querySelector("#myRange");
+var output = document.querySelector("#demo");
 
 function makeRows(rowNum) {
-    for(let r = 0; r < rowNum; r++) {
-        for(let nr = 0; nr < rowNum; nr++) {
-            let row = document.createElement("div");
-            container.appendChild(row).className = "rows";
-        }
+    const squareSize = container.clientWidth / rowNum; 
+
+    for (let i = 0; i < rowNum * rowNum; i++) {
+        const row = document.createElement("div");
+        row.className = "rows";
+        row.style.width = `${squareSize}px`;
+        row.style.height = `${squareSize}px`;
+        container.appendChild(row);
     }
 }
-makeRows(5);
+
+output.textContent = `${slider.value} x ${slider.value}`; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    if(slider.oninput == 1) {
+        output.textContent = `${slider.value}`;
+    } else {
+        output.textContent = this.value + " x " + this.value;
+}
+} 
+
+makeRows(8);
